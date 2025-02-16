4ddf8052cd6bcbdaf9e7c3fdedf0aa1a3054034c
Lê Minh Trí <trilm189@gmail.com>
2025-02-16 15:18:42 +0700

    "fig layout"

---
+++ SkinTime.BLL/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,21 @@
+﻿using SkinTime.DAL.Entities;
+using SkinTime.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinTime.BLL.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IUnitOfWork _unitOfWork ;
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork; 
+        }
+
+        public async Task<IReadOnlyList<ServiceCategory>> GetAllCaterory() => await _unitOfWork.Repository<ServiceCategory>().ListAllAsync();
+    }
+}
---
+++ SkinTime.MVC/Models/CategoriesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SkinTime.MVC.Models
+{
+    public class CategoriesModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
